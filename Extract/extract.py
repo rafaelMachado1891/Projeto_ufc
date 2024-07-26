c3879fbb5c1a8b1178ea7c6a9e7305467e7989ed
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-25 23:22:12 -0300

    atualizando projeto

--- Extract/extract.py
+++ Extract/extract.py
@@ -1,6 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+def coletar_dados_div(soup, div_class, stats_texts):
+    """
+    Coleta dados de uma div específica com base na classe e textos das estatísticas.
+    
+    :param soup: Objeto BeautifulSoup da página web.
+    :param div_class: Classe da div que contém as estatísticas.
+    :param stats_texts: Dicionário onde as chaves são os textos que identificam as estatísticas e os valores são os nomes das variáveis para armazená-los.
+    :return: Dicionário com os dados coletados.
+    """
+    div = soup.find('div', class_=div_class)
+    dados = {key: None for key in stats_texts.values()}
+    
+    if div:
+        stats = div.find_all('div', class_='athlete-stats__stat')
+        for stat in stats:
+            stat_value = stat.find('p', class_='athlete-stats__text athlete-stats__stat-numb').text.strip()
+            stat_text = stat.find('p', class_='athlete-stats__text athlete-stats__stat-text').text.strip()
+            for key, value in stats_texts.items():
+                if key in stat_text:
+                    dados[value] = stat_value
+                    break
+    
+    return dados
+
 url = "https://www.ufc.com.br/athlete/alex-pereira"
 
 response = requests.get(url)
@@ -8,26 +32,17 @@ response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Encontrar a div principal com a classe 'hero-profile'
-    resultado = soup.find_all('div', class_="hero-profile")
-
-    # Encontrar a div com a classe 'c-overlap__stats-wrap'
-    precisao_strike = soup.find('div', class_='c-overlap__stats-wrap')
-
-    golpes_conectados = None
-    golpes_desferidos = None
+    # Dicionário com os textos das estatísticas e os nomes das variáveis
+    stats_texts = {
+        'Wins by Knockout': 'vitorias_por_nocaute',
+        'First Round Finishes': 'vitorias_primeiro_round'
+    }
     
-    if precisao_strike:
-        stats = precisao_strike.find_all('dl', class_='c-overlap__stats')
-        for stat in stats:
-            stat_text = stat.find('dt', class_='c-overlap__stats-text').text.strip()
-            stat_value = stat.find('dd', class_='c-overlap__stats-value').text.strip()
-            if 'Golpes Sig. Conectados' in stat_text:
-                golpes_conectados = stat_value
-            elif 'Golpes Sig. Desferidos' in stat_text:
-                golpes_desferidos = stat_value
+    # Coletar dados das estatísticas
+    estatisticas = coletar_dados_div(soup, 'stats-records-inner', stats_texts)
 
     data = []
+    resultado = soup.find_all('div', class_="hero-profile")
     for result in resultado:
         categoria = result.find('p', class_="hero-profile__division-title").text.strip() if result.find('p', class_="hero-profile__division-title") else None
         atleta = result.find('h1', class_="hero-profile__name").text.strip() if result.find('h1', class_="hero-profile__name") else None
@@ -42,8 +57,8 @@ if response.status_code == 200:
             'Recorde': record,
             'Ultima_Luta': ultima_luta,
             'Foto': imagem_src,
-            'Golpes Sig. Conectados': golpes_conectados,
-            'Golpes Sig. Desferidos': golpes_desferidos
+            'Vitorias por Nocaute': estatisticas['vitorias_por_nocaute'],
+            'Vitorias no Primeiro Round': estatisticas['vitorias_primeiro_round']
         })
 
     if data:
